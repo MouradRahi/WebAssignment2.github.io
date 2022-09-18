3ce78546510bafca9d4e4ef15df95a68a5e20bbb
Mourad Rahi <mouradrahidf1234d@gmail.com>
2022-09-18 13:12:43 +0300

    third commit

--- game.js
+++ game.js
@@ -1,9 +1,21 @@
-let pointer_x=-1;
-let pointer_y=-1;
-document.onmousemove = function(event) {
-	pointer_x = event.pageX;
-	pointer_y = event.pageY;
-}
-if (pointer_x!=1){
-    console.log("movement");
-}
+window.onload= ()=>{
+	let pointer_x=-1;
+	let pointer_y=-1;
+	document.getElementById("start").onmousemove =  function(event) {myFunction(event)};
+
+	function myFunction(e) {
+	  pointer_x = e.clientX;
+	  pointer_y = e.clientY;
+	  let coor = "Coordinates: (" + pointer_x + "," + pointer_y + ")";
+	  console.log(coor);
+	  if((pointer_x>740 || pointer_x <240) && (pointer_y>426 || pointer_y < 126)){
+		console.log("lose");
+	}
+	}
+	
+	const boundaries=document.getElementsByClassName("boundary");
+	console.log(boundaries[0].pageX);
+	if ((pointer_x==boundaries[0].pageX && pointer_y==boundaries[0].pageY)){
+		console.log("lose");
+	}
+} 
