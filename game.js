8dde5893a4e40328c11922c2d677b076901e45da
Mourad Rahi <mouradrahidf1234d@gmail.com>
2022-09-18 13:36:12 +0300

    third commit

--- game.js
+++ game.js
@@ -1,21 +1,36 @@
 window.onload= ()=>{
-	let pointer_x=-1;
-	let pointer_y=-1;
+	let pointer_x=500;
+	let pointer_y=150;
+	const win_condition=true;
+	const score =0;
 	document.getElementById("start").onmousemove =  function(event) {myFunction(event)};
 
 	function myFunction(e) {
-	  pointer_x = e.clientX;
-	  pointer_y = e.clientY;
-	  let coor = "Coordinates: (" + pointer_x + "," + pointer_y + ")";
-	  console.log(coor);
-	  if((pointer_x>740 || pointer_x <240) && (pointer_y>426 || pointer_y < 126)){
-		console.log("lose");
-	}
+	  	pointer_x = e.clientX;
+	  	pointer_y = e.clientY;
+	  	let coor = "Coordinates: (" + pointer_x + "," + pointer_y + ")";
+	  	console.log(coor);
+	  	
 	}
 	
+	
 	const boundaries=document.getElementsByClassName("boundary");
-	console.log(boundaries[0].pageX);
-	if ((pointer_x==boundaries[0].pageX && pointer_y==boundaries[0].pageY)){
-		console.log("lose");
+	
+	boundaries[0].onmousemove = function(event) {func (event)}
+	boundaries[1].onmousemove = function(event) {func (event)}
+	boundaries[2].onmousemove = function(event) {func (event)}
+	boundaries[3].onmousemove = function(event) {func (event)}
+	boundaries[4].onmousemove = function(event) {func (event)}
+	
+	
+	function func(even){
+		document.getElementById("status").innerText="you lose";
+		boundaries[0].style.backgroundColor="red";
+		boundaries[1].style.backgroundColor="red";
+		boundaries[2].style.backgroundColor="red";
+		boundaries[3].style.backgroundColor="red";
+		boundaries[4].style.backgroundColor="red";
+		
 	}
+	
 } 
