9dfd6ad5c71f9265f5a6446f177346f9b85fbb31
Mourad Rahi <mouradrahidf1234d@gmail.com>
2022-09-18 15:20:57 +0300

    eith commit

--- game.js
+++ game.js
@@ -2,11 +2,11 @@ window.onload= ()=>{
 	let pointer_x=500;
 	let pointer_y=150;
 	const win_condition=true;
-	let score =0;
+	
 	document.getElementById("start").onmousemove =  function(event) {myFunction(event)};
 
 	function myFunction(e) {
-		document.getElementById("status").innerText="Game started your current score is: "+score;
+		document.getElementById("status").innerText="Game started your current score is: ";
 	  	pointer_x = e.clientX;
 	  	pointer_y = e.clientY;
 	  	let coor = "Coordinates: (" + pointer_x + "," + pointer_y + ")";
@@ -14,41 +14,43 @@ window.onload= ()=>{
 	  	
 	}
 
-	if (document.getElementById("status").value!="You lose your current score is: "+score-10){
+	
+	if (document.getElementById("status").value!="You lose your current score is: "){
 
 		document.getElementById("end").onmousemove =  function(event) {myFunc(event)};
 
 		function myFunc(e) {
 			score=score+5;
-			document.getElementById("status").innerText="You win your current score is: "+score;
-	  		pointer_x = e.clientX;
-	  		pointer_y = e.clientY;
-	  	//let coor = "Coordinates: (" + pointer_x + "," + pointer_y + ")";
-	  	//console.log(coor);
-	  	
+			document.getElementById("status").innerText="You win your current score is: ";
+			pointer_x = e.clientX;
+			pointer_y = e.clientY;
+			//let coor = "Coordinates: (" + pointer_x + "," + pointer_y + ")";
+			//console.log(coor);
+	
 		}
 	}
-	
-	
+
+
 	const boundaries=document.getElementsByClassName("boundary");
-	
+
 	boundaries[0].onmousemove = function(event) {func (event)}
 	boundaries[1].onmousemove = function(event) {func (event)}
 	boundaries[2].onmousemove = function(event) {func (event)}
 	boundaries[3].onmousemove = function(event) {func (event)}
 	boundaries[4].onmousemove = function(event) {func (event)}
-	
-	
+
+
 	function func(even){
-		score=score-10;
+		
 		//console.log(score);
-		document.getElementById("status").innerText="You lose your current score is: "+score;
+		document.getElementById("status").innerText="You lose your current score is: ";
 		boundaries[0].style.backgroundColor="red";
 		boundaries[1].style.backgroundColor="red";
 		boundaries[2].style.backgroundColor="red";
 		boundaries[3].style.backgroundColor="red";
 		boundaries[4].style.backgroundColor="red";
-		
+	
 	}
+
 	
 } 
